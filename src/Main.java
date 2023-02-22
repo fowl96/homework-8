35dc56c5987bcd82429bbe748932026495eba279
fowl96User <fowl69@yandex.ru>
2023-02-22 03:31:22 +0300

    дз8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,104 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+
+
+
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int [] first = new int [3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+        double [] second = {1.57, 7.654, 9.986};
+        int [] third = new int [12];
+        third[2] = 2;
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] first = new int[3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+        double[] second = {1.57, 7.654, 9.986};
+        int[] third = new int[12];
+        third[2] = 2;
+        for (int i = 0; i < first.length; i++) {
+            System.out.print(first[i]);
+            if (i < (first.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < second.length; i++) {
+            System.out.print(second[i]);
+            if (i < (second.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < third.length; i++) {
+            System.out.print(third[i]);
+            if (i < (third.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] first = new int[3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+        double[] second = {1.57, 7.654, 9.986};
+        int[] third = new int[12];
+        third[2] = 2;
+        for (int i = 0; i < first.length; i++) {
+            System.out.print(first[2-i]);
+            if (i < (first.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < second.length; i++) {
+            System.out.print(second[2-i]);
+            if (i < (second.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < third.length; i++) {
+            System.out.print(third[11-i]);
+            if (i < (third.length - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int[] first = new int[3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] % 2 != 0){
+                first[i]++;
+            }
+            System.out.print(first[i]+ " ");
+        }
+        System.out.println();
+    }
+
 }
